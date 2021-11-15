a750cf504bc5e332f01dde06c0581ebef7b1b57c
Az-s <azim.sa@yahoo.com>
2021-11-15 14:11:03 +0600

    some changes in actions hw86

--- frontend/src/store/actions/usersActions.js
+++ frontend/src/store/actions/usersActions.js
@@ -1,6 +1,7 @@
 import axiosApi from "../../axiosApi";
-import {historyPush} from './historyActions';
-import {toast} from "react-toastify";
+import { historyPush } from './historyActions';
+import { toast } from "react-toastify";
+import {push} from 'connected-react-router';
 
 export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS';
 export const SIGNUP_USER_FAILURE = 'SIGNUP_USER_FAILURE';
@@ -10,6 +11,7 @@ export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
 export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE';
 
 export const LOGOUT_USER = 'LOGOUT_USER';
+export const GET_HISTORY_SUCCESS = 'GET_HISTORY_SUCCESS';
 
 export const signupUserSuccess = user => ({ type: SIGNUP_USER_SUCCESS, payload: user });
 export const signupUserFailure = error => ({ type: SIGNUP_USER_FAILURE, payload: error });
@@ -17,6 +19,8 @@ export const signupUserFailure = error => ({ type: SIGNUP_USER_FAILURE, payload:
 export const loginUserRequest = () => ({ type: LOGIN_USER_REQUEST });
 export const loginUserSuccess = user => ({ type: LOGIN_USER_SUCCESS, payload: user });
 export const loginUserFailure = error => ({ type: LOGIN_USER_FAILURE, payload: error });
+
+const getHistorySuccess = history => ({ type: GET_HISTORY_SUCCESS, history });
 
 export const SignUpUser = userData => {
     return async dispatch => {
@@ -54,12 +58,43 @@ export const SignInUser = userData => {
 
 export const logoutUser = () => {
     return async (dispatch, getState) => {
-      const headers = {
-        'Authorization': getState().users.user.token,
-      };
-  
-      await axiosApi.delete('/users/sessions', {headers});
-      dispatch({type: LOGOUT_USER});
-      dispatch(historyPush('/'));
+        const headers = {
+            'Authorization': getState().users.user.token,
+        };
+
+        await axiosApi.delete('/users/sessions', { headers });
+        dispatch({ type: LOGOUT_USER });
+        dispatch(historyPush('/'));
     };
-  };
+};
+
+export const saveTrack = trackId => {
+    return async (dispatch, getState) => {
+        try {
+            const state = getState();
+            axiosApi.post('/tracks_history', { trackId: trackId }, { headers: { "Authorization": state.users.token } });
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data)
+            } else {
+                dispatch(loginUserFailure({ global: "No network connection " }))
+            }
+        }
+    };
+};
+
+export const getHistory = () => {
+    return async (dispatch, getState) => {
+        try {
+            const state = getState();
+            const response = axiosApi.get('/tracks_history', { headers: { "Authorization": state.users.token } });
+            dispatch(getHistorySuccess(response.data));
+        } catch (error) {
+            if (error.response) {
+                dispatch(push('/'));
+            } else {
+                dispatch(loginUserFailure({ global: "No network connection " }))
+            }
+        }
+    };
+};
