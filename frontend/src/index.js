8f4d75ce1f8e0df8336afee458724f9ea2c165ab
Az-s <azim.sa@yahoo.com>
2021-11-26 17:23:53 +0600

    add theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter  } from "react-router-dom";
-import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import authorReducer from './store/reducers/authorReducer';
-import albumReducer from './store/reducers/albumReducer';
-import usersReducer from './store/reducers/usersREducer';
+import { ToastContainer } from 'react-toastify';
+import theme from './theme';
+import store from './store/configureStore';
 import App from './App';
 import './index.css';
 
-const rootReducer = combineReducers({
-  authors: authorReducer,
-  albums: albumReducer,
-  users: usersReducer,
-});
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk)
-));
-
 const app = (
   <Provider store={store}>
-    <BrowserRouter >
-      <App />
+    <BrowserRouter history={history}>
+      <ThemeProvider theme={theme}>
+        <ToastContainer />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>
 );
